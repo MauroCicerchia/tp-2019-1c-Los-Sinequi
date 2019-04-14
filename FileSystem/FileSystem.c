f05b84df13034930b38f898df50322d948b64344
MauroCicerchia <maurocicerchia98@gmail.com>
2019-04-14 17:53:13 -0300

    Procesos actualizados

--- FileSystem/FileSystem.c
+++ FileSystem/FileSystem.c
@@ -2,8 +2,83 @@
 
 int main(int argc, char **argv) {
 
+	t_log *logger = NULL;
+	char *input;
 
-	printf("Hola");
+	iniciar_logger(&logger);
+
+	input = readline(">");
+
+	while(strcmp("", input)) {
+
+		processQuery(input, logger);
+		free(input);
+		input = readline(">");
+
+	}
+
+	log_destroy(logger);
 
 	return 0;
 }
+
+e_query processQuery(char *query, t_log *logger) {
+
+	char log_msg[100];
+	e_query queryType;
+
+	char **args = string_split(query, " ");
+
+	queryType = getQueryType(args[0]);
+
+	switch(queryType) {
+
+		case QUERY_SELECT:
+
+			//select(args[1], args[2]);
+
+			sprintf(log_msg, "Recibi un SELECT %s %s", args[1], args[2]);
+
+			break;
+
+		case QUERY_INSERT:
+
+			//insert(args[1], args[2], args[3], args[4]);
+
+			sprintf(log_msg, "Recibi un INSERT %s %s %s", args[1], args[2], args[3]);
+
+			break;
+
+		case QUERY_CREATE:
+
+			//create(args[1], args[2], args[3], args[4]);
+
+			sprintf(log_msg, "Recibi un CREATE %s %s %s %s", args[1], args[2], args[3], args[4]);
+
+			break;
+
+		case QUERY_DESCRIBE:
+
+			//describe(args[1]);
+
+			sprintf(log_msg, "Recibi un DESCRIBE %s", args[1]);
+
+			break;
+
+		case QUERY_DROP:
+
+			//drop(args[1]);
+
+			sprintf(log_msg, "Recibi un DROP %s", args[1]);
+
+			break;
+
+		default:
+			return queryError(logger);
+
+	}
+
+	log_info(logger, log_msg);
+	return queryType;
+}
+
--- FileSystem/FileSystem.h
+++ FileSystem/FileSystem.h
@@ -1,7 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include<commons/log.h>
 #include<commons/string.h>
 #include<commons/config.h>
 #include<readline/readline.h>
-#include "../libs/utils.h"
+#include"console.h"
+
+e_query processQuery(char *, t_log*);
+
+void iniciar_logger(t_log **logger)
+{
+	*logger = log_create("FileSystem.log", "FileSystem", 1, LOG_LEVEL_INFO);
+}
