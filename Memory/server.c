a3aab799d75f13dc0b5cd199a271e339090c814c
MauroCicerchia <maurocicerchia98@gmail.com>
2019-04-15 22:46:12 -0300

    Creado servidor en proceso Memory

---
+++ Memory/server.c
@@ -0,0 +1,47 @@
+#include"server.h"
+
+int createServer() {
+
+	struct addrinfo hints;
+	struct addrinfo *serverInfo;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;		// No importa si uso IPv4 o IPv6
+	hints.ai_flags = AI_PASSIVE;		// Asigna el address del localhost: 127.0.0.1
+	hints.ai_socktype = SOCK_STREAM;	// Indica que usaremos el protocolo TCP
+
+	getaddrinfo(NULL, PUERTO, &hints, &serverInfo);
+
+	int listeningSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);
+
+	bind(listeningSocket,serverInfo->ai_addr, serverInfo->ai_addrlen);
+	freeaddrinfo(serverInfo);
+
+	listen(listeningSocket, BACKLOG);
+
+	return listeningSocket;
+}
+
+void waitClient(int listeningSocket) {
+	struct sockaddr_in addr;			// Esta estructura contendra los datos de la conexion del cliente. IP, puerto, etc.
+	socklen_t addrlen = sizeof(addr);
+
+	int socketCliente = accept(listeningSocket, (struct sockaddr *) &addr, &addrlen);
+
+	char package[PACKAGESIZE];
+	int status = 1;
+
+	printf("Cliente conectado. Esperando mensajes:\n");
+
+	while (status != 0){
+		status = recv(socketCliente, (void*) package, PACKAGESIZE, 0);
+		if (status != 0) printf("%s", package);
+
+	}
+
+	close(socketCliente);
+}
+
+void closeServer(int server) {
+	close(server);
+}
