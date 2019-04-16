aaa74fb1aef2b58af8b65f7bef8c96cd7f90d3e4
MauroCicerchia <maurocicerchia98@gmail.com>
2019-04-15 22:59:11 -0300

    Creado cliente en proceso Kernel

---
+++ Kernel/client.c
@@ -0,0 +1,36 @@
+#include"client.h"
+
+int connectToServer() {
+	struct addrinfo hints;
+	struct addrinfo *serverInfo;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+
+	getaddrinfo(IP, PUERTO, &hints, &serverInfo);	// Carga en serverInfo los datos de la conexion
+
+	int serverSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);
+
+	connect(serverSocket, serverInfo->ai_addr, serverInfo->ai_addrlen);
+	freeaddrinfo(serverInfo);
+
+	return serverSocket;
+}
+
+int sendMessages(int serverSocket) {
+	int enviar = 1;
+	char message[PACKAGESIZE];
+
+	printf("Conectado al servidor. Bienvenido al sistema, ya puede enviar mensajes. Escriba 'exit' para salir\n");
+
+	while(enviar){
+		fgets(message, PACKAGESIZE, stdin);
+		if (!strcmp(message,"exit\n")) enviar = 0;			// Chequeo que el usuario no quiera salir
+		if (enviar) send(serverSocket, message, strlen(message) + 1, 0); 	// Solo envio si el usuario no quiere salir.
+	}
+}
+
+void closeConnection(int serverSocket) {
+	close(serverSocket);
+}
