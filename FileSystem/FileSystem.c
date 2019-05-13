e37e0ff81627d224f7878b1e1adfd9d24bbeca90
Fluzko <fluzko@gmail.com>
2019-05-13 15:48:43 -0300

    planteo de resolucion select e insert, falta implmentar funciones

--- FileSystem/FileSystem.c
+++ FileSystem/FileSystem.c
@@ -1,11 +1,19 @@
 #include "FileSystem.h"
 
+typedef struct{
+	int timeStamp;
+	char *key;
+	char *value;
+}data;
+
+
+
 int main(int argc, char **argv) {
 
 	t_log *logger = NULL;
 
 	iniciar_logger(&logger);
-	iniciar_servidor(logger);
+//	iniciar_servidor(logger);
 	start_API(logger);
 
 	log_destroy(logger);
@@ -13,7 +21,7 @@ int main(int argc, char **argv) {
 	return 0;
 }
 
-e_query processQuery(char *query, t_log *logger) {
+char* processQuery(char *query, t_log *logger) {
 
 	char log_msg[100];
 	e_query queryType;
@@ -31,7 +39,7 @@ e_query processQuery(char *query, t_log *logger) {
 
 		case QUERY_SELECT:
 
-			//select(args[1], args[2]);
+			qselect(args[1], args[2]);
 
 			sprintf(log_msg, "Recibi un SELECT %s %s", args[1], args[2]);
 
@@ -39,7 +47,7 @@ e_query processQuery(char *query, t_log *logger) {
 
 		case QUERY_INSERT:
 
-			//insert(args[1], args[2], args[3], args[4]);
+			qinsert(args[1], args[2], args[3], args[4]);
 
 			sprintf(log_msg, "Recibi un INSERT %s %s %s", args[1], args[2], args[3]);
 
@@ -77,4 +85,69 @@ e_query processQuery(char *query, t_log *logger) {
 	log_info(logger, log_msg);
 	return queryType;
 }
+
+void qinsert(char *table, int key, char *value, int timeStamp){
+
+	char *url = string_append("tables/", table);
+	url = string_append(url, ".bin");
+	FILE *file = txt_open_for_append(url);
+	free(url);
+	char *insert = toLFSmode(timeStamp,key,value);
+	txt_write_in_file(file, insert);
+	free(insert);
+	txt_close_file(file);
+
+}
+
+char *qselect(char *table, int key){
+
+	char *url = string_append("tables/", table);
+	url = string_append(url, ".bin");
+	FILE *file = fopen(url,"r");
+
+	t_list *list = list_create();
+
+	loadList(list,file);
+	txt_close_file(file);
+	toDATAmode(list);
+	char *value = getValue(list,key);
+
+	list_destroy(list);
+	return value;
+}
+
+void loadList(t_list *list,FILE file){
+
+}
+
+char *getValue(t_list list,int key){
+
+}
+
+data *toDATAmode(t_list *list){
+
+}
+
+char *toLFSmode(int timeStamp, int key, char *value){
+
+	char *insert = string_append(timeStamp, ";");
+	char *sKey = string_itoa(key);
+	insert = string_append(insert, sKey);
+	free(sKey);
+	insert = string_append(insert, ";");
+	insert = string_append(insert, value);
+	insert = string_append(insert, "\n");
+	return insert;
+}
+
+//si un puntero apunta a tal direccion de memoria(0x1) y cambio a donde apunta (0x2), la antigua posicion(0x1) entiendo que la perdi, pero.. queda libre para que se use? o hay que hacerle free?
+
+/*
+ * 	char *value = getValue(list,key);
+ *
+ *	list_destroy(list);
+ *	return value;
+ *
+ *	como hago el free de value?
+*/
 
