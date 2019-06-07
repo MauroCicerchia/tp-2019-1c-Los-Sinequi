2cca03f1240d1031253a7044e46dd879dc01a8c1
Fluzko <fluzko@gmail.com>
2019-06-06 23:34:58 -0300

    agrego los /debug para tester en server

---
+++ sharedLib/Debug/sharedLib/subdir.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../sharedLib/client.c \
+../sharedLib/consistency.c \
+../sharedLib/console.c \
+../sharedLib/packaging.c \
+../sharedLib/process.c \
+../sharedLib/query.c \
+../sharedLib/server.c 
+
+OBJS += \
+./sharedLib/client.o \
+./sharedLib/consistency.o \
+./sharedLib/console.o \
+./sharedLib/packaging.o \
+./sharedLib/process.o \
+./sharedLib/query.o \
+./sharedLib/server.o 
+
+C_DEPS += \
+./sharedLib/client.d \
+./sharedLib/consistency.d \
+./sharedLib/console.d \
+./sharedLib/packaging.d \
+./sharedLib/process.d \
+./sharedLib/query.d \
+./sharedLib/server.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+sharedLib/%.o: ../sharedLib/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
