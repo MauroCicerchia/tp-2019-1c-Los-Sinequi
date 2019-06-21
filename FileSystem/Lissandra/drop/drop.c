0c3831d7bc8d4342770eebea442736039cd7c11c
Fluzko <fluzko@gmail.com>
2019-06-21 12:30:28 -0300

    agrego funcionalidades de drop

--- FileSystem/Lissandra/drop/drop.c
+++ FileSystem/Lissandra/drop/drop.c
@@ -1,6 +1,6 @@
 #include "drop.h"
 
-int qdrop(char *table)
+int qdrop(char *table) //agregar semaforo de drop para que nadie toque las tablas meintras se dropean
 {
 	if(!fs_tableExists(table)){
 		log_error(logger,"La tabla que se intenta droppear no existe");
@@ -43,5 +43,59 @@ void deleteDirectoriesAndFiles(char *table)
 
 void freeBlocks(char *table)
 {
+	char *tableUrl = makeTableUrl(table);
+	t_list *files = getFiles(tableUrl);
+	char *fileUrl, *blockUrl, *strBlocks;
+	char **blocks;
+	int iBlock;
+	FILE *f;
+
+	for(int i = 0; i < list_size(files); i++){ //recorro archivo por archivo
+		fileUrl = string_duplicate(tableUrl);
+		string_append(&fileUrl,list_get(files,i));
+
+		strBlocks = getListOfBlocks(fileUrl);
+		blocks = string_get_string_as_array(strBlocks);
+
+		for(int j = 0; j < sizeofArray(blocks); j++){ //borro bloque por bloque
+			blockUrl = fs_getBlocksUrl();
+			string_append(&blockUrl,blocks[j]);
+			string_append(&blockUrl,".bin");
+			f = fopen(blockUrl,"w"); //borro el contenido del bloque
+			fclose(f);
+
+			free(blockUrl);
+
+			iBlock = strtol(blocks[j],NULL,10);
+			ba_freeBlock(iBlock); //libero en bloque en el bitarray
+		}
+
+		free(fileUrl); free(strBlocks);
+		free(blocks);
+	}
+}
+
+//devuelve lista con tmps y las particiones
+t_list *getFiles(char *tableUrl)
+{
+	t_list *toReturn = list_create();
+
+	DIR *d;
+	struct  dirent *dir;
+
+	d = opendir(tableUrl);
+	dir = readdir(d);
+	while(dir != NULL){
+		if( strcmp(dir->d_name,".") &&
+			strcmp(dir->d_name,"..")&&
+			strcmp(dir->d_name,"Metadata.bin"))
+		list_add(toReturn,dir->d_name);
+	}
+	closedir(d);
+	return toReturn;
+}
+
+//saca la tabla de la lista de tablas activas en memoria
+void deleteTableFromMemory(char *table){
 
 }
--- FileSystem/Lissandra/drop/drop.h
+++ FileSystem/Lissandra/drop/drop.h
@@ -1,6 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<unistd.h>
 
 #include"../../Fs/fs.h"
+#include"../../Fs/blocks.h"
+
+#ifndef DROP_H
+#define DROP_H
 
 int qdrop(char *table);
+void deleteDirectoriesAndFiles(char *table);
+void freeBlocks(char *table);
+void deleteTableFromMemory(char *table);
+t_list *getFiles(char *tableUrl);
+
+#endif
