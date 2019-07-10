43f611655de55f7f41490e2725efd2154962cd9e
Fluzko <fluzko@gmail.com>
2019-07-10 15:51:52 -0300

    deploy cambiado

--- Deploy.sh
+++ Deploy.sh
@@ -1,33 +1,123 @@
 #!/bin/bash
 cd
 
-if test ! -d /home/utnso/so-commons-library; then
-        git clone https://github.com/sisoputnfrba/so-commons-library.git
-        cd so-commons-library/
-        sudo make install
+if test -d /home/utnso/so-commons-library; then
+    cd /home/utnso/so-commons-library/
+    sudo make uninstall
+    cd ..
+    sudo rm -r ./so-commons-library
 fi
 
 cd
 
-if test ! -d /home/utnso/lissandra-checkpoint/; then
-	mkdir /home/utnso/lissandra-checkpoint/
+test ! -d /home/utnso/so-commons-library; then
+        git clone https://github.com/sisoputnfrba/so-commons-library.git
+        cd so-commons-library/
+        sudo make install
+
+cd
+
+# -------------
+# directorio para prueba de stress
+if test -d /home/utnso/lfs-stress/; then
+	sudo rm -r /home/utnso/lfs-stress
 fi
 
-if test ! -d /home/utnso/lissandra-checkpoint/Metadata/; then
-    mkdir /home/utnso/lissandra-checkpoint/Metadata/
+cd
+mkdir -p /home/utnso/lfs-stress
+cd /home/utnso/lfs-stress/
+mkdir Metadata
+mkdir Blocks
+mkdir Tables
+
+cd Metadata/
+echo -e "BLOCK_SIZE=128\nBLOCKS=8192\nMAGIC_NUMBER=LISSANDRA" >> Metadata.bin
+
+# -------------
+
+cd
+
+# -------------
+# directorio para prueba de Memoria
+if test -d /home/utnso/lfs-prueba-memoria/; then
+	sudo rm -r /home/utnso/lfs-prueba-memoria
 fi
 
-if test ! -d /home/utnso/lissandra-checkpoint/Metadata/metadata.bin; then
-    touch /home/utnso/lissandra-checkpoint/Metadata/metadata.bin
+cd
+mkdir -p /home/utnso/lfs-prueba-memoria
+cd /home/utnso/lfs-prueba-memoria/
+mkdir Metadata
+mkdir Blocks
+mkdir Tables
+
+cd Metadata/
+echo -e "BLOCK_SIZE=64\nBLOCKS=4096\nMAGIC_NUMBER=LISSANDRA" >> Metadata.bin
+
+# -------------
+
+cd
+
+# -------------
+# directorio para prueba de lfs
+if test -d /home/utnso/lfs-compactacion/; then
+	sudo rm -r /home/utnso/lfs-compactacion
 fi
 
-if test ! -d /home/utnso/lissandra-checkpoint/Tables/; then
-    mkdir /home/utnso/lissandra-checkpoint/Tables/
+cd
+mkdir -p /home/utnso/lfs-compactacion
+cd /home/utnso/lfs-compactacion/
+mkdir Metadata
+mkdir Blocks
+mkdir Tables
+
+cd Metadata/
+echo -e "BLOCK_SIZE=64\nBLOCKS=4096\nMAGIC_NUMBER=LISSANDRA" >> Metadata.bin
+
+# -------------
+
+cd
+
+# -------------
+# directorio para prueba de Kernel
+if test -d /home/utnso/lfs-prueba-kernel/; then
+	sudo rm -r /home/utnso/lfs-prueba-kernel
 fi
 
-if test ! -d /home/utnso/lissandra-checkpoint/Blocks/; then
-    mkdir /home/utnso/lissandra-checkpoint/Blocks/
+cd
+mkdir -p /home/utnso/lfs-prueba-kernel
+cd /home/utnso/lfs-prueba-kernel/
+mkdir Metadata
+mkdir Blocks
+mkdir Tables
+
+cd Metadata/
+echo -e "BLOCK_SIZE=128\nBLOCKS=4096\nMAGIC_NUMBER=LISSANDRA" >> Metadata.bin
+
+# -------------
+cd
+
+# -------------
+# directorio para prueba de Kernel
+if test -d /home/utnso/lfs-base/; then
+	sudo rm -r /home/utnso/lfs-base
 fi
+
+cd
+mkdir -p /home/utnso/lfs-base
+cd /home/utnso/lfs-base/
+mkdir Metadata
+mkdir Blocks
+mkdir Tables
+
+cd Metadata/
+echo -e "BLOCK_SIZE=64\nBLOCKS=4096\nMAGIC_NUMBER=LISSANDRA" >> Metadata.bin
+
+# -------------
+
+cd
+
+# -------------
+#Deploy de los 3 sistemas
 
 if test ! -d /home/utnso/workspace; then
     mkdir /home/utnso/workspace
