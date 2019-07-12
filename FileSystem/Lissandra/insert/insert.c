344f5a9187f1b706dafb2b001f9d456d473119b2
MacarenaPesce <maqarena.agostina.pesce@gmail.com>
2019-07-12 20:46:29 -0300

    insert,mas comentarios

--- FileSystem/Lissandra/insert/insert.c
+++ FileSystem/Lissandra/insert/insert.c
@@ -1,25 +1,33 @@
-#include"insert.h"
+#include"inse|rt.h"
 
 int qinsert(char *table, char* key, char *value, char* timeStamp){
 	delayer();
 
-	if(strlen(value) > valueSize){
+	if(strlen(value) > valueSize){ //el valor a insertar supera la longitud ya definida
 		log_error(logger,"[INSERT]: El value supera la longitud definida");
+
 		return 0;
 	}
 
 	log_info(logger, "[INSERT]: Chequeando existencia de la tabla..");
 
-	if(!fs_tableExists(table)){
+	//si el valor no supera, chequeo la existencia de la tabla sobre la que hacemos el insert
+	if(!fs_tableExists(table)){//la tabla no existe
 		log_error(logger, "[INSERT]: La tabla sobre la que se quiere hacer el insert no existe");
+
 		return 0;
-	}else log_info(logger, "[INSERT]: La tabla existe");
+	} //si existe la tabla
+	else log_info(logger, "[INSERT]: La tabla existe");
 
 		log_info(logger,"[INSERT]: Insertando en memtable..");
-		sem_wait(&MUTEX_MEMTABLE);
+
+		//inserto en la memetable
+		sem_wait(&MUTEX_MEMTABLE); //bloqueo memtable mientras hace la asignacion
 		mt_insert(table,timeStamp,key,value);
 		sem_post(&MUTEX_MEMTABLE);
+
 		log_info(logger, "[INSERT]: Insert Exitoso");
+
 //		dump();
 		return 1;
 	}
