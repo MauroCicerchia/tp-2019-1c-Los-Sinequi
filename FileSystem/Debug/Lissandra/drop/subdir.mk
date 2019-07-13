04f0461eb153390507c3dc81eca4e28431b557b0
MauroCicerchia <maurocicerchia98@gmail.com>
2019-07-13 06:22:13 -0300

    C

---
+++ FileSystem/Debug/Lissandra/drop/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Lissandra/drop/drop.c 
+
+OBJS += \
+./Lissandra/drop/drop.o 
+
+C_DEPS += \
+./Lissandra/drop/drop.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Lissandra/drop/%.o: ../Lissandra/drop/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -I"/home/utnso/workspace/tp-2019-1c-Los-Sinequi/sharedLib" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
