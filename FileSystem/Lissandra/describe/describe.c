44fc1d66056c7d85b8e01f5f71a05f0be361083d
MacarenaPesce <maqarena.agostina.pesce@gmail.com>
2019-07-12 23:00:01 -0300

    comeeentarios describe

--- FileSystem/Lissandra/describe/describe.c
+++ FileSystem/Lissandra/describe/describe.c
@@ -3,18 +3,25 @@
 metadata *qdescribe(char *table)
 {
 	delayer();
+
 	log_info(logger, "[DESCRIBE]: Chequeando existencia de la tabla...");
+
+	//verifico la existencia de la tabla
 	if(!fs_tableExists(table)){
 		log_error(logger,"[DESCRIBE]: No existe la tabla sobre la cual se quiere hacer describe");
-		return NULL;
+
+		return NULL; //no existe la tabla
 	}
+
+	//existe la tabla
 	log_info(logger, "[DESCRIBE]: La tabla existe");
 
 	log_info(logger, "[DESCRIBE]: Leyendo info..");
+	//leo la info de metadata de la tabla
 	metadata *tableMetadata = fs_getTableMetadata(table);
 
-	if(tableMetadata != NULL)
+	if(tableMetadata != NULL) //verificamos la lectura de la info
 		log_info(logger, "[DESCRIBE]: Info leida con exito");
 
-	return tableMetadata;
+	return tableMetadata; //devuelvo la metadata de la tabla pedida
 }
